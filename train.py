239babac150625d4c6e5b5625b3290eab79a9362
Kunyu He <kunyuheecon@gmail.com>
2019-04-18 23:53:31 -0500

    Finalize on HW2

--- train.py
+++ train.py
@@ -74,8 +74,8 @@ def load_features():
     """
     Xs = np.load(INPUT_DIR + 'X.npz')
     ys = np.load(INPUT_DIR + 'y.npz')
-    X_train, X_test = Xs['train'], Xs['test']
-    y_train, y_test = ys['train'], ys['test']
+    X_train, X_test = Xs['train'], None
+    y_train, y_test = ys['train'], None
 
     return X_train, X_test, y_train, y_test
 
