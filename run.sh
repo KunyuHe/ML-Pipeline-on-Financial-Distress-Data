11247f55f570a0f222a5360923a8a01c03c4b58a
Kunyu He <kunyuheecon@gmail.com>
2019-04-19 00:04:37 -0500

    Updated shell script

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+pip install --user -r requirements.txt
+
 python etl.py
-
 start https://mybinder.org/v2/gh/KunyuHe/ML-Pipeline-on-Financial-Distress-Data/master?filepath=.%2FEDA%2FData%20Exploration.ipynb
-
 python featureEngineering.py
-
 python train.py
